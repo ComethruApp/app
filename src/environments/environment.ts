f5b56abc3bdf6cb043a9b8ab824adbbd226c9b22
ErikBoesen <me@erikboesen.com>
2019-08-30 16:45:45 -0400

    Install firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyCiOYzjTzlkdUGTl9m7qKw5tCqqucVCVts",
+        authDomain: "nightu-cd362.firebaseapp.com",
+        databaseURL: "https://nightu-cd362.firebaseio.com",
+        projectId: "nightu-cd362",
+        storageBucket: "",
+        messagingSenderId: "394469709890",
+        appId: "1:394469709890:web:64a14333402c3d4c",
+    },
 };
 
 /*
