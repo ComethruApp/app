f7344e26c33ce37ac8dd8add0b3a046f9c09c159
ErikBoesen <me@erikboesen.com>
2019-09-04 22:47:28 -0400

    Disable image picker

--- src/app/new-event/new-event.page.ts
+++ src/app/new-event/new-event.page.ts
@@ -3,7 +3,7 @@ import { FirebaseService } from '../services/firebase.service';
 import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { LoadingController, ToastController } from '@ionic/angular';
 import { Router } from '@angular/router';
-import { ImagePicker } from '@ionic-native/image-picker/ngx';
+//import { ImagePicker } from '@ionic-native/image-picker/ngx';
 import { WebView } from '@ionic-native/ionic-webview/ngx';
 
 
@@ -15,10 +15,10 @@ import { WebView } from '@ionic-native/ionic-webview/ngx';
 export class NewEventPage implements OnInit {
 
   validations_form: FormGroup;
-  image: any;
+  //image: any;
 
   constructor(
-    private imagePicker: ImagePicker,
+    //private imagePicker: ImagePicker,
     public toastCtrl: ToastController,
     public loadingCtrl: LoadingController,
     public router: Router,
@@ -32,7 +32,7 @@ export class NewEventPage implements OnInit {
   }
 
   resetFields(){
-    this.image = "./assets/imgs/default_image.jpg";
+    //this.image = "./assets/imgs/default_image.jpg";
     this.validations_form = this.formBuilder.group({
       title: new FormControl('', Validators.required),
       description: new FormControl('', Validators.required)
@@ -43,7 +43,7 @@ export class NewEventPage implements OnInit {
     let data = {
       title: value.title,
       description: value.description,
-      image: this.image
+      //image: this.image,
     }
     this.firebaseService.createEvent(data)
     .then(
@@ -52,7 +52,7 @@ export class NewEventPage implements OnInit {
       }
     )
   }
-
+  /*
   openImagePicker(){
     this.imagePicker.hasReadPermission()
     .then((result) => {
@@ -98,6 +98,7 @@ export class NewEventPage implements OnInit {
       console.log(err);
     })
   }
+  */
 
   async presentLoading(loading) {
     return await loading.present();
