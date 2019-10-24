362a3f4debc4176a0b82102b588ed00d2c74d4ad
ErikBoesen <me@erikboesen.com>
2019-10-23 23:59:20 -0400

    Add to auth interface

--- src/app/services/auth/auth.module.ts
+++ src/app/services/auth/auth.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { IonicStorageModule } from '@ionic/storage';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule,
+    FormsModule,
+    IonicStorageModule.forRoot(),
   ]
 })
 export class AuthModule { }
