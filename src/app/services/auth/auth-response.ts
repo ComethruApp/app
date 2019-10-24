8ade8b11e673a25eb2a026338e304723701ec5fe
ErikBoesen <me@erikboesen.com>
2019-10-24 11:22:45 -0400

    Add auth service

--- src/app/services/auth/auth-response.ts
+++ src/app/services/auth/auth-response.ts
@@ -1,2 +1,9 @@
 export interface AuthResponse {
+    user: {
+        id: number,
+        name: string,
+        email: string,
+        access_token: string,
+        expires_in: number,
+    },
 }
