2f08bcbb36866e8bffd04ddbaff2fea582216490
ErikBoesen <me@erikboesen.com>
2019-10-24 18:54:31 -0400

    Add initial login page

--- src/app/pages/auth/login/login.page.ts
+++ src/app/pages/auth/login/login.page.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { AuthService } from '../../../services/auth/auth.service';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+    selector: 'app-login',
+    templateUrl: './login.page.html',
+    styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+    constructor(private authService: AuthService, private router: Router) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    login(form){
+        this.authService.login(form.value).subscribe((res)=>{
+            this.router.navigateByUrl('home');
+        });
+    }
 
 }
