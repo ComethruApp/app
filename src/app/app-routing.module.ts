acdfc488c902354451bb4a3cb1919aab0ac8cd71
ErikBoesen <me@erikboesen.com>
2019-10-24 11:40:37 -0400

    Add tabs back

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,9 +6,11 @@ const routes: Routes = [
   { path: 'register', loadChildren: './pages/auth/register/register.module#RegisterPageModule' },
 
   { path: 'tabs', loadChildren: './pages/tabs/tabs.module#TabsPageModule' },
+  /*
   { path: 'details/:id', loadChildren: './pages/details/details.module#DetailsPageModule' },
   { path: 'feed', loadChildren: './pages/feed/feed.module#FeedPageModule' },
   { path: 'new-event', loadChildren: './pages/new-event/new-event.module#NewEventPageModule' },
+    */
   // { path: 'new-event-modal', loadChildren: './new-event-modal/new-event-modal.module#NewEventModalPageModule' },
 ];
 
--- src/app/pages/tabs/tabs.module.ts
+++ src/app/pages/tabs/tabs.module.ts
@@ -12,10 +12,12 @@ const routes: Routes = [
     path: '',
     component: TabsPage,
     children:[
+        /*
         { path: 'feed', loadChildren: '../feed/feed.module#FeedPageModule' },
         { path: 'map', loadChildren: '../map/map.module#MapPageModule' },
         { path: 'new-event', loadChildren: '../new-event/new-event.module#NewEventPageModule' },
         { path: 'profile', loadChildren: '../profile/profile.module#ProfilePageModule' },
+       */
     ]
   },
   {
