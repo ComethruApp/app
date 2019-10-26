db238360676b019cac18ef61254fd8187710f84b
ErikBoesen <me@erikboesen.com>
2019-10-25 23:49:22 -0400

    Add new event form

--- src/app/pages/form-event/form-event.page.ts
+++ src/app/pages/form-event/form-event.page.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { APIService } from '../../services/api/api.service';
 
 @Component({
-  selector: 'app-form-event',
-  templateUrl: './form-event.page.html',
-  styleUrls: ['./form-event.page.scss'],
+    selector: 'app-form-event',
+    templateUrl: './form-event.page.html',
+    styleUrls: ['./form-event.page.scss'],
 })
 export class FormEventPage implements OnInit {
 
-  constructor() { }
+    constructor(
+        private apiService: APIService,
+        private router: Router
+    ) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    submit(form) {
+        // TODO: also support update!
+        this.apiService.createEvent(form.value).subscribe((res)=>{
+            this.router.navigateByUrl('tabs');
+        });
+    }
 
 }
