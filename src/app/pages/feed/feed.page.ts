5788328c72891b57ca3d7700183448352721133a
ErikBoesen <me@erikboesen.com>
2019-10-26 12:26:46 -0400

    Clean up feed code

--- src/app/pages/feed/feed.page.ts
+++ src/app/pages/feed/feed.page.ts
@@ -4,39 +4,43 @@ import { LoadingController } from '@ionic/angular';
 import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-feed',
-  templateUrl: './feed.page.html',
-  styleUrls: ['./feed.page.scss'],
+    selector: 'app-feed',
+    templateUrl: './feed.page.html',
+    styleUrls: ['./feed.page.scss'],
 })
 export class FeedPage implements OnInit {
-  events: Event[];
+    events: Event[];
 
-  constructor(
-    public loadingCtrl: LoadingController,
-    private apiService: APIService,
-    private router: Router,
-    private route: ActivatedRoute,
-  ) { }
+    constructor(
+        public loadingCtrl: LoadingController,
+        private apiService: APIService,
+        private router: Router,
+        private route: ActivatedRoute,
+    ) { }
 
-  ngOnInit() {
-    if (this.route && this.route.data) {
-      this.getData();
+    ngOnInit() {
+        if (this.route && this.route.data) {
+            this.getData();
+        }
     }
-  }
 
-  async getData(){
-    const loading = await this.loadingCtrl.create({
-      message: 'Loading...'
-    });
-    this.presentLoading(loading);
+    async getData(){
+        const loading = await this.loadingCtrl.create({
+            message: 'Loading...'
+        });
+        this.presentLoading(loading);
 
-    this.apiService.getEvents().subscribe(events => {
-        loading.dismiss();
-        this.events = events;
-    })
-  }
+        this.apiService.getEvents().subscribe(events => {
+            loading.dismiss();
+            this.events = events;
+        })
+    }
 
-  async presentLoading(loading) {
-    return await loading.present();
-  }
+    goToEvent(eventId) {
+
+    }
+
+    async presentLoading(loading) {
+        return await loading.present();
+    }
 }
