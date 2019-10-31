d64289ac24a1d673836fce5e9dd19fdb0b725da0
ErikBoesen <me@erikboesen.com>
2019-10-31 19:00:13 -0400

    Fix status bar background

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,7 +26,7 @@ export class AppComponent {
     initializeApp() {
         this.platform.ready().then(() => {
             this.platform.ready().then(() => {
-                this.statusBar.styleDefault();
+                this.statusBar.backgroundColorByHexString('#263238');
                 this.splashScreen.hide();
             });
         });
