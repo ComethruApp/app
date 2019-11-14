a7e06e08b1ce685033d58a0c5b378c752eed87e4
ErikBoesen <me@erikboesen.com>
2019-11-13 22:39:25 -0500

    Add loading and fail explanation for login activity

--- src/app/pages/auth/login/login.page.ts
+++ src/app/pages/auth/login/login.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
 import { AuthService } from '../../../services/auth/auth.service';
 import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
     selector: 'app-login',
@@ -16,6 +18,8 @@ export class LoginPage implements OnInit {
         private authService: AuthService,
         private router: Router,
         private formBuilder: FormBuilder,
+        public alertController: AlertController,
+        public loadingCtrl: LoadingController,
     ) { }
 
     ngOnInit() {
@@ -29,12 +33,29 @@ export class LoginPage implements OnInit {
         });
     }
 
-    login(form){
+    async login(form){
+        const loading = await this.loadingCtrl.create({
+            message: 'Logging in...'
+        });
+        this.presentLoading(loading);
         let data = form.value;
-        this.authService.login(data).subscribe((res)=>{
+        this.authService.login(data).subscribe(response =>{
             this.resetFields();
+            loading.dismiss();
             this.router.navigateByUrl('tabs');
+        }, response => {
+            loading.dismiss();
+            this.alertController.create({
+                header: 'Error',
+                message: response.error.message,
+                buttons: ['OK'],
+            }).then(alert => {
+                alert.present();
+            });
         });
     }
 
+    async presentLoading(loading) {
+        return await loading.present();
+    }
 }
