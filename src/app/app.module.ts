24097fc5d3a6b1d737e13d95c0301628f30826ff
ErikBoesen <me@erikboesen.com>
2019-11-25 17:31:47 -0500

    Add initial UserList component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,10 +19,12 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { NativeGeocoder } from '@ionic-native/native-geocoder/ngx';
 import { BackgroundGeolocation } from '@ionic-native/background-geolocation/ngx';
+import { UserListComponent } from './components/user-list/user-list.component';
 
 @NgModule({
   declarations: [
       AppComponent,
+      UserListComponent,
   ],
   entryComponents: [
   ],
