e32409a96aa58400b78071c8e77b1539c1988f54
ErikBoesen <me@erikboesen.com>
2019-11-25 20:14:06 -0500

    Generate friend-buttons component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { UserListComponent } from './user-list/user-list.component';
+import { FriendButtonsComponent } from './friend-buttons/friend-buttons.component';
 
 @NgModule({
     declarations: [
-        UserListComponent
+        UserListComponent,
+        FriendButtonsComponent
     ],
     imports: [
         IonicModule,
---
+++ src/app/components/friend-buttons/friend-buttons.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-friend-buttons',
+  templateUrl: './friend-buttons.component.html',
+  styleUrls: ['./friend-buttons.component.scss']
+})
+export class FriendButtonsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
