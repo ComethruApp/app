31835c6c3d268d1bfb94f2621e5f7b6ea509d64e
ErikBoesen <me@erikboesen.com>
2019-11-25 23:58:03 -0500

    Move content of events list to component

--- src/app/components/event-list/event-list.component.ts
+++ src/app/components/event-list/event-list.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-event-list',
-  templateUrl: './event-list.component.html',
-  styleUrls: ['./event-list.component.scss']
+    selector: 'app-event-list',
+    templateUrl: './event-list.component.html',
+    styleUrls: ['./event-list.component.scss']
 })
 export class EventListComponent implements OnInit {
+    @Input() events: Event;
+    @Input() emptyMessage: string = "No events";
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
--- src/app/pages/feed/feed.module.ts
+++ src/app/pages/feed/feed.module.ts
@@ -1,27 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 
 import { IonicModule } from '@ionic/angular';
 
 import { FeedPage } from './feed.page';
 
+import { ComponentsModule } from '../../components/components.module';
+
 const routes: Routes = [
-  {
-    path: '',
-    component: FeedPage,
-  }
+    {
+        path: '',
+        component: FeedPage,
+    }
 ];
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    IonicModule,
-    RouterModule.forChild(routes)
-  ],
-  declarations: [FeedPage],
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        ComponentsModule,
+        RouterModule.forChild(routes)
+    ],
+    declarations: [FeedPage],
 })
 export class FeedPageModule {}
