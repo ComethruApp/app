b0de69d21ce62732ef858721eaf20188b86186b1
ErikBoesen <me@erikboesen.com>
2019-11-25 20:22:27 -0500

    Move friendship buttons to separate component

--- src/app/components/friend-buttons/friend-buttons.component.ts
+++ src/app/components/friend-buttons/friend-buttons.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { APIService } from '../../services/api/api.service';
+import { User } from '../../services/api/models';
 
 @Component({
-  selector: 'app-friend-buttons',
-  templateUrl: './friend-buttons.component.html',
-  styleUrls: ['./friend-buttons.component.scss']
+    selector: 'app-friend-buttons',
+    templateUrl: './friend-buttons.component.html',
+    styleUrls: ['./friend-buttons.component.scss']
 })
 export class FriendButtonsComponent implements OnInit {
+    @Input() user: User;
 
-  constructor() { }
+    constructor(
+        private api: APIService,
+    ) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() { }
 
+    requestFriend() {
+        this.api.requestFriend(this.user.id).subscribe(response => {
+            this.user.has_received_friend_request = true;
+        });
+    }
+
+    cancelRequest() {
+        this.api.cancelRequest(this.user.id).subscribe(response => {
+            this.user.has_received_friend_request = false;
+        });
+    }
+
+    acceptRequest() {
+        this.api.acceptRequest(this.user.id).subscribe(response => {
+            this.user.has_sent_friend_request = false;
+            this.user.is_friend = true;
+        });
+    }
+    rejectRequest() {
+        this.api.rejectRequest(this.user.id).subscribe(response => {
+            this.user.has_sent_friend_request = false;
+        });
+    }
+
+    unfriend() {
+        this.api.unfriend(this.user.id).subscribe(response => {
+            this.user.is_friend = false;
+        });
+    }
 }
--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { APIService } from '../../services/api/api.service';
 import { User } from '../../services/api/models';
 
 @Component({
@@ -12,39 +11,7 @@ export class UserListComponent implements OnInit {
     @Input() emptyMessage: string;
 
     constructor(
-        private api: APIService,
     ) { }
 
     ngOnInit() { }
-
-    // TODO: this indexing method feels really unclean. Works for now, but would be nice to clean it up later.
-    requestFriend(i) {
-        this.api.requestFriend(this.users[i].id).subscribe(response => {
-            this.users[i].has_received_friend_request = true;
-        });
-    }
-
-    cancelRequest(i) {
-        this.api.cancelRequest(this.users[i].id).subscribe(response => {
-            this.users[i].has_received_friend_request = false;
-        });
-    }
-
-    acceptRequest(i) {
-        this.api.acceptRequest(this.users[i].id).subscribe(response => {
-            this.users[i].has_sent_friend_request = false;
-            this.users[i].is_friend = true;
-        });
-    }
-    rejectRequest(i) {
-        this.api.rejectRequest(this.users[i].id).subscribe(response => {
-            this.users[i].has_sent_friend_request = false;
-        });
-    }
-
-    unfriend(i) {
-        this.api.unfriend(this.users[i].id).subscribe(response => {
-            this.users[i].is_friend = false;
-        });
-    }
 }
