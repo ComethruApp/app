3e98d09699dad20f85679ab311c1f94c82157f99
ErikBoesen <me@erikboesen.com>
2019-11-26 21:04:54 -0500

    Add more error checking for event-list component

--- src/app/components/event-list/event-list.component.ts
+++ src/app/components/event-list/event-list.component.ts
@@ -1,21 +1,22 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { Event_ } from '../../services/api/models';
+
 @Component({
     selector: 'app-event-list',
     templateUrl: './event-list.component.html',
     styleUrls: ['./event-list.component.scss']
 })
 export class EventListComponent implements OnInit {
-    @Input() events: Event;
-    @Input() emptyMessage: string = "No events";
+    @Input() events: Event_[];
+    @Input() emptyMessage: string = "No events.";
 
     constructor(
         private router: Router,
     ) { }
 
-    ngOnInit() {
-    }
+    ngOnInit() { }
 
     goToEvent(eventId) {
         this.router.navigate(['/event/' + eventId]);
