6ebd075591a17166251c809a54cff3d9c4b6c11b
ErikBoesen <me@erikboesen.com>
2019-11-26 21:47:26 -0500

    Finish moon component

--- src/app/components/moon/moon.component.ts
+++ src/app/components/moon/moon.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-moon',
-  templateUrl: './moon.component.html',
-  styleUrls: ['./moon.component.scss']
+    selector: 'app-moon',
+    templateUrl: './moon.component.html',
+    styleUrls: ['./moon.component.scss']
 })
 export class MoonComponent implements OnInit {
+    @Input() rating: string;
 
-  constructor() { }
+    @Input() large: boolean = false;
+    private size: number;
 
-  ngOnInit() {
-  }
+    constructor() { }
+
+    ngOnInit() {
+    }
 
 }
