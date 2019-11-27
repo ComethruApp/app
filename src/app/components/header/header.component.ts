c04616351ea163a8ea30b91475574bca6f718d4f
ErikBoesen <me@erikboesen.com>
2019-11-27 11:50:08 -0500

    Remove + button on feed page

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -8,7 +8,6 @@ import { Component, OnInit, Input } from '@angular/core';
 export class HeaderComponent implements OnInit {
     @Input() title: string = '';
     @Input() back: boolean = false;
-    @Input() newEvent: boolean = false;
 
     constructor() { }
 
