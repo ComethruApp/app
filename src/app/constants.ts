be27cec5a7fe1b3ad69e9f081ba19ff8361951a6
ErikBoesen <me@erikboesen.com>
2019-11-27 21:46:26 -0500

    Use cloud server

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,2 +1,2 @@
-//export const HOST = 'https://comethru.herokuapp.com';
-export const HOST = 'http://localhost:5000';
+export const HOST = 'https://comethru.herokuapp.com';
+//export const HOST = 'http://localhost:5000';
