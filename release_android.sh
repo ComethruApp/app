8e50b0c482dbc9cafbff9ec317060f6ac91a7f81
ErikBoesen <me@erikboesen.com>
2019-11-28 22:21:36 -0500

    Remove old apk in release script

--- release_android.sh
+++ release_android.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
+signed_apk="Comethru.apk"
+rm -f $signed_apk
 ionic cordova build android --prod --release
 unsigned_apk="platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk"
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $HOME/comethru.jks $unsigned_apk comethru
-signed_apk="Comethru.apk"
 zipalign -v 4 $unsigned_apk $signed_apk
 apksigner verify $signed_apk && echo "All's well!" || echo "Signing failed."
