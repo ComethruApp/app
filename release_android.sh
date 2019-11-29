967e48b2c4159733aed56f112befc54659682934
ErikBoesen <me@erikboesen.com>
2019-11-28 20:58:28 -0500

    Add android release script

---
+++ release_android.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+ionic cordova build android --prod --release
+unsigned_apk="platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk"
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $HOME/comethru.jks $unsigned_apk comethru
+signed_apk="Comethru.apk"
+zipalign -v 4 $unsigned_apk $signed_apk
+apksigner verify $signed_apk && echo "All's well!" || echo "Signing failed."
