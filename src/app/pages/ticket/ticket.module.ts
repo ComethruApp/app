3390deec9fdea5c448aa19f37ab7dce71e3f9a2f
ErikBoesen <me@erikboesen.com>
2019-11-29 22:19:39 -0500

    Fix ticket module

--- src/app/pages/ticket/ticket.module.ts
+++ src/app/pages/ticket/ticket.module.ts
@@ -1,20 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 
 import { IonicModule } from '@ionic/angular';
 
-import { TicketPageRoutingModule } from './ticket-routing.module';
-
 import { TicketPage } from './ticket.page';
 
+import { ComponentsModule } from '../../components/components.module';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: TicketPage
+    }
+];
+
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    TicketPageRoutingModule
-  ],
-  declarations: [TicketPage]
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        ComponentsModule,
+        RouterModule.forChild(routes)
+    ],
+    declarations: [TicketPage]
 })
 export class TicketPageModule {}
