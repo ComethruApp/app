25191cb057ee24aaa64265b70392e94ec26a0df6
ErikBoesen <me@erikboesen.com>
2019-12-08 23:10:01 -0500

    Show Facebook avatars

--- src/app/components/avatar/avatar.component.ts
+++ src/app/components/avatar/avatar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { User } from '../../services/api/models';
 
 @Component({
     selector: 'app-avatar',
@@ -6,11 +7,17 @@ import { Component, OnInit, Input } from '@angular/core';
     styleUrls: ['./avatar.component.scss']
 })
 export class AvatarComponent implements OnInit {
-    @Input() avatar: string;
+    @Input() user: User;
     @Input() large: boolean = false;
+    avatar: string;
 
     constructor() { }
 
     ngOnInit() {
+        if (this.user.facebook_id) {
+            this.avatar = 'https://graph.facebook.com/' + this.user.facebook_id + '/picture?type=large'
+        } else {
+            this.avatar = '/assets/imgs/unknown.png';
+        }
     }
 }
