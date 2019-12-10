15119a123c8c222a5b9280aec49654bd377a27ab
ErikBoesen <me@erikboesen.com>
2019-12-10 18:28:24 -0500

    Actually hide text on loader when asked

--- src/app/components/loader/loader.component.ts
+++ src/app/components/loader/loader.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-loader',
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoaderComponent implements OnInit {
     message: string;
-    showMessage: boolean = true;
+    @Input() showMessage: boolean = true;
 
     constructor(
     ) { }
