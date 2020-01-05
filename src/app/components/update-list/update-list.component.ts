2ac218091157dcdc364ea7842204fd82d5ef9b1b
ErikBoesen <me@erikboesen.com>
2020-01-04 23:34:24 -0700

    Use update-list component

--- src/app/components/update-list/update-list.component.ts
+++ src/app/components/update-list/update-list.component.ts
@@ -1,14 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Update } from '../../services/api/models';
 
 @Component({
-  selector: 'app-update-list',
-  templateUrl: './update-list.component.html',
-  styleUrls: ['./update-list.component.scss'],
+    selector: 'app-update-list',
+    templateUrl: './update-list.component.html',
+    styleUrls: ['./update-list.component.scss'],
 })
 export class UpdateListComponent implements OnInit {
+    @Input() users: User[];
 
-  constructor() { }
+    constructor(
+        private router: Router,
+    ) { }
 
-  ngOnInit() {}
+    ngOnInit() {}
 
+    openUser(userId: number) {
+        this.router.navigate(['/user/' + userId]);
+    }
+
+    openUpdate(updateId: number) {
+        this.router.navigate(['/event/' + this.eventId + '/updates/' + updateId]);
+    }
 }
