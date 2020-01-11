c78c0ab8180f391e6cd66d8dc37e61645d5aad12
ErikBoesen <me@erikboesen.com>
2020-01-11 16:15:16 -0500

    Restructure reviews page to be useful

--- src/app/pages/reviews/reviews.page.ts
+++ src/app/pages/reviews/reviews.page.ts
@@ -11,6 +11,9 @@ import { Review } from '../../services/api/models';
 export class ReviewsPage implements OnInit {
     id: number;
     reviews: Review[] = null;
+    positiveCount: number;
+    neutralCount: number;
+    negativeCount: number;
 
     constructor(
         private route: ActivatedRoute,
@@ -24,7 +27,25 @@ export class ReviewsPage implements OnInit {
 
     async getData() {
         this.api.getEventReviews(this.id).subscribe(reviews => {
-            this.reviews = reviews;
+            let positiveReviews: Review[] = [];
+            let  neutralReviews: Review[] = [];
+            let negativeReviews: Review[] = [];
+            this.positiveCount = 0;
+            this.neutralCount = 0;
+            this.negativeCount = 0;
+            for (let review of reviews) {
+                if (review.positive && !review.negative) {
+                    this.positiveCount++;
+                    if (review.body) positiveReviews.push(review);
+                } else if (!review.positive && !review.negative) {
+                    this.neutralCount++;
+                    if (review.body) neutralReviews.push(review);
+                } else if (!review.positive && review.negative) {
+                    this.negativeCount++;
+                    if (review.body) negativeReviews.push(review);
+                }
+            }
+            this.reviews = positiveReviews.concat(neutralReviews).concat(negativeReviews);
         });
     }
 
