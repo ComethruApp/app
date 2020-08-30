ebaa4d9d73f069bb86eb42e920ab1969828cd70b
ErikBoesen <me@erikboesen.com>
2020-08-30 13:24:41 -0600

    Support empty dates in date service

--- src/app/services/dates/dates.service.ts
+++ src/app/services/dates/dates.service.ts
@@ -9,30 +9,31 @@ export class DatesService {
     constructor() { }
 
     /* Convert from raw ISO-formatted UTC date into a date in local time that can be formatted. */
-    private process(date) {
-        return moment.utc(date).local();
+    private process(date, format) {
+        if (date == undefined) return null;
+        return moment.utc(date).local().format(format);
     }
 
     iso(date) {
-        return this.process(date).format();
+        return this.process(date, '');
     }
     day(date) {
-        return this.process(date).format('dddd');
+        return this.process(date, 'dddd');
     }
     date(date) {
-        return this.process(date).format('M/D');
+        return this.process(date, 'M/D');
     }
     fullDate(date) {
-        return this.process(date).format('dddd, M/D');
+        return this.process(date, 'dddd, M/D');
     }
     time(date) {
-        return this.process(date).format('h:mm');
+        return this.process(date, 'h:mm');
     }
     fullTime(date) {
-        return this.process(date).format('h:mma');
+        return this.process(date, 'h:mma');
     }
     meridiem(date) {
-        return this.process(date).format('a');
+        return this.process(date, 'a');
     }
 
 }
